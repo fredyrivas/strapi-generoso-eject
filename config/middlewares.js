23a188a6a1610a125c7e0129087bd48a5a3f183f
Fredy Rivas <fredy.rivas@mediamonks.com>
2025-03-28 14:05:02 -0600

    set cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,18 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
