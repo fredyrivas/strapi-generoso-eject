53bd3b47f7196f88b6819f9cba7b359ea78251d2
fredyrivas <fredyrivasss@gmail.com>
2025-09-30 16:31:52 -0600

    update plugins

--- config/plugins.js
+++ config/plugins.js
@@ -40,33 +40,3 @@ module.exports = ({ env }) => {
   };
 };
 
-
-
-
-
-
-
-
-
-// module.exports = ({ env }) => ({
-//   "users-permissions": {
-//     config: {
-//       jwtSecret: env("JWT_SECRET"),
-//     },
-//   },
-//   upload: {
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//         cloud_name: env("CLOUDINARY_NAME"),
-//         api_key: env("CLOUDINARY_KEY"),
-//         api_secret: env("CLOUDINARY_SECRET"),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-//   },
-// });
-
