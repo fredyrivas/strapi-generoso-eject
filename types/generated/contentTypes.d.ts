983c152258aeb9ca95193b723d9030b440b1336d
fredyrivas <fredyrivasss@gmail.com>
2026-08-13 19:41:26 -0600

    add enabledCosting to purchase-item

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -815,6 +815,7 @@ export interface ApiPurchaseItemPurchaseItem
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    enabledCosting: Schema.Attribute.Boolean;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
